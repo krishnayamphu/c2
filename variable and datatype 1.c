de2f0858c1f520d45dbb4c9618fe0ddf5ada5fc9
KrishnaYamphu <krishnayamphu@gmail.com>
2023-03-19 14:16:43 +0545

    int project

---
+++ variable and datatype 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a=5;
+int b=10;
+int sum=0;
+
+sum=a+b;
+printf("Total sum: %d",sum);
+return 0;
+}
---
+++ variable and datatype.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int a=5;
+int b=10;
+float temp=20.987654;
+char ch='a';
+char name[]="Ram";
+
+printf("Value of a and b: %d and %d",a,b);
+printf("\nTemperature value: %.2f",temp);
+printf("\n%c",ch);
+printf("\nName: %s",name);
+return 0;
+}
